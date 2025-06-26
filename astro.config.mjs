00f901bcc11464efc8c3c9a199c4620285079f20
Pancham <75246178+Pancham555@users.noreply.github.com>
2025-06-26 22:12:47 +0530

    fix: update adapter runtime & use rewrites in vercel.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
+import vercel from '@astrojs/vercel/serverless';
 import icon from "astro-icon";
 
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output:'server',
   integrations: [mdx(), sitemap(), tailwind(), icon()],
-  adapter: netlify(),
+  adapter: vercel({}),
 });
